1a0b21390de881ad850833654f88978af41e220d
storkdeveloper@gmail.com <storkdeveloper@gmail.com>
2021-08-21 15:10:06 -0300

    code organization

--- brazilian_soccer.py
+++ brazilian_soccer.py
@@ -1,75 +1,102 @@
+import pathlib
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
 
-def get_number_data(is_data):
-    try:
-        return int(is_data.text.strip())
-    except ValueError:
-        pass
-
-
 class Brasileiro:
-    def __init__(self, year, series):
-        if type(year) is not int:
-            raise TypeError("year value must be 'int'")
-        elif year < 2012:
-            raise ValueError("year must be greater than 2012")
-        elif series.lower() not in ["a", "b"]:
+    def __init__(self, year: int, series: str) -> None:
+        if year < 2012:
+            raise ValueError('year must be greater than 2012')
+        elif series.lower() not in ['a', 'b']:
             raise ValueError("series must be 'A' or 'B'")
 
         self.year = year
-        self.teams = 20
         self.series = series.lower()
 
-    def get_data(self):
-        url = ("https://www.cbf.com.br/futebol-brasileiro/competicoes/"
-               f"campeonato-brasileiro-serie-{self.series}/{self.year}")
-        table = requests.get(url)
-        soup = BeautifulSoup(table.content, "html.parser")
+        self._df = pd.DataFrame()
+        self._url = (
+            'https://www.cbf.com.br/futebol-brasileiro/competicoes/'
+            f'campeonato-brasileiro-serie-{self.series}/{self.year}'
+        )
+        self._response = requests.get(self._url)
+        self._soup = BeautifulSoup(self._response.content, 'html.parser')
 
-        rank_html = soup.find_all(name="span", attrs={"class": "hidden-xs"})
-        ranks = [rank.text for rank in rank_html]
+    def _get_teams(self) -> list:
+        teams_html = self._soup.find_all(
+            name='span', attrs={'class': 'hidden-xs'}
+        )
+        teams = [team.text for team in teams_html]
 
-        points_html = soup.find_all(name="th",
-                                    attrs={"class": None, "scope": "row"})
+        return teams
+
+    def _get_points(self) -> list:
+        points_html = self._soup.find_all(
+            name='th', attrs={'class': None, 'scope': 'row'}
+        )
         points = [int(point.text) for point in points_html]
 
-        all_points = [x for x in map(get_number_data, soup.find_all(name="td"))
-                      if x is not None]
+        return points
 
-        values = []
-        for n in range(10, 201, 10):
-            values.append(all_points[0 if n == 10 else n - 10: n])
+    def _get_matches(self) -> list:
+        matches_html = self._soup.find_all(
+            name='td', attrs={'class': None}
+        )
+        matches_text = []
 
-        self._df = pd.DataFrame({"Position": range(1, self.teams + 1),
-                                 "Team": ranks,
-                                 "Points": points,
-                                 "Games": [val[0] for val in values],
-                                 "Wins": [val[1] for val in values],
-                                 "Draws": [val[2] for val in values],
-                                 "Defeats": [val[3] for val in values],
-                                 "Scored Goals": [val[4] for val in values],
-                                 "Against Goals": [val[5] for val in values],
-                                 "Goal Balance": [val[6] for val in values],
-                                 "Yellow Cards": [val[7] for val in values],
-                                 "Red Cards": [val[8] for val in values],
-                                 "Points %": [val[9] for val in values]})
+        for item in matches_html:
+            try:
+                matches_text.append(int(item.text))
+            except ValueError:
+                # If the item can not be converted to 'int', it is useless
+                # so we simply 'pass'
+                pass
 
-    def show_data(self):
-        try:
-            return self._df.head(self.teams)
-        except AttributeError:
-            print(None)
+        matches = [list(n) for n in zip(*[iter(matches_text)] * 4)]
 
-    def save_data(self, file_name, file_format="xlsx"):
-        if file_format not in ["xlsx", "csv"]:
-            raise ValueError("available file formats: 'xlsx' and 'csv'")
-        try:
-            if file_format == "xlsx":
-                self._df.to_excel(f"{file_name}.{file_format}", index=False)
-            elif file_format == "csv":
-                self._df.to_csv(f"{file_name}.{file_format}", index=False)
-        except AttributeError:
-            print(None)
+        return matches
+
+    def _get_others(self) -> list:
+        others_html = self._soup.find_all(
+            name='td', attrs={'class': 'hidden-xs'}
+        )
+        others_text = [other.text for other in others_html]
+        others = [list(n) for n in zip(*[iter(others_text)] * 6)]
+
+        return others
+
+    def get_data(self) -> None:
+        teams = self._get_teams()
+        points = self._get_points()
+        matches = self._get_matches()
+        others = self._get_others()
+
+        info = {
+            'Position': range(1, len(teams) + 1),
+            'Team': teams,
+            'Points': points,
+            'Games': [n[0] for n in matches],
+            'Wins': [n[1] for n in matches],
+            'Draws': [n[2] for n in matches],
+            'Defeats': [n[3] for n in matches],
+            'Scored Goals': [n[0] for n in others],
+            'Against Goals': [n[1] for n in others],
+            'Goal Balance': [n[2] for n in others],
+            'Yellow Cards': [n[3] for n in others],
+            'Red Cards': [n[4] for n in others],
+            'Points %': [n[5] for n in others],
+        }
+
+        self._df = pd.DataFrame(data=info)
+
+    def show_data(self) -> None:
+        teams = self._df.shape[0]
+        print(self._df.head(teams))
+
+    def save_data(self, path: str) -> None:
+        file = pathlib.Path(path)
+
+        if file.suffix == '.csv':
+            self._df.to_csv(path, index=False)
+        elif file.suffix == '.xlsx':
+            self._df.to_excel(path, index=False)
